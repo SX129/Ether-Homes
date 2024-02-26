8802fa0c7683155e6e680433aeabf0a6170a365b
Henry Nguyen <nguyenhenry682@gmail.com>
2024-02-26 17:13:10 -0600

    added list function

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-// For the ERC721 token
+// For the ERC721 NFT token
 interface IERC721 {
     function transferFrom(
         address _from,
@@ -10,11 +10,17 @@ interface IERC721 {
     ) external;
 }
 
+// Escrow contract to hold property NFTs during sale process
 contract Escrow {
     address public lender;
     address public inspector;
     address payable public seller;
     address public nftAddress;
+
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => address) public buyer;
+    mapping(uint256 => uint256) public escrowAmount;
 
     constructor(
         address _lender,
@@ -26,5 +32,14 @@ contract Escrow {
         inspector = _inspector;
         seller = _seller;
         nftAddress = _nftAddress;
+    }
+    
+    function list(uint256 _nftID) public{
+
+        // Tranfer property NFT from seller wallet into escrow
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        // Mark property NFT as listed
+        isListed[_nftID] = true;
     }
 }
