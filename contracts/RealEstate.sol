ed306620dc1f4b6b56414701a49d493b7cfc9985
Henry Nguyen <nguyenhenry682@gmail.com>
2024-02-25 23:45:32 -0600

    added mint function for nfts

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,27 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract RealEstate {
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+// Inherit from ERC721URIStorage allows for quick NFT creation
+contract RealEstate is ERC721URIStorage {
+    // Creates enumerable ERC721 tokens
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("RealEstate", "RE") {}
+
+    // Mint function to create new property NFTs
+    function mint(string memory tokenURI) public returns(uint256){
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
 
 }
