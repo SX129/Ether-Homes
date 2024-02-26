f5a6ac7f2c2c2812a8a5c78652a074a44b6ec024
Henry Nguyen <nguyenhenry682@gmail.com>
2024-02-26 00:01:39 -0600

    added mint function and setup signers

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,10 +6,22 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    let buyer;
+    let seller;
+    let realEstate;
+
     it('Saves the addresses', async () => {
+
+        // Setup accounts
+        [buyer, seller] = await ethers.getSigners();
+
+        // Deploy RealEstate contract
         const RealEstate = await ethers.getContractFactory('RealEstate');
         realEstate = await RealEstate.deploy();
 
-        console.log(realEstate.address);
+        // Mint function with meta data
+        let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS")
+        await transaction.wait();
+
     });
 })
