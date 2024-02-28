e5449bbec51bd20a1da24c6b7eb609cb2973ae45
Henry Nguyen <nguyenhenry682@gmail.com>
2024-02-27 23:49:02 -0600

    added script to deploy smart contracts onto blockchian

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,54 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether');
+}
+
 async function main() {
+  // Setup accounts
+  [lender, inspector, seller, buyer] = await ethers.getSigners();
+
+  // Deploy Real Estate contract
+  const RealEstate = await ethers.getContractFactory('RealEstate');
+  const realEstate = await RealEstate.deploy();
+  await realEstate.deployed();
+
+  console.log(`RealEstate deployed to: ${realEstate.address}`);
+
+  // Loop to Mint property NFTs on IPFS
+  for (let i = 0; i < 3; i++) {
+    const transaction = await realEstate.connect(seller).mint(`https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/${i + 1}.json`);
+    await transaction.wait();
+  }
+
+  // Deploy Escrow contract
+  const Escrow = await ethers.getContractFactory('Escrow');
+  escrow = await Escrow.deploy(
+      lender.address,
+      inspector.address,
+      seller.address,
+      realEstate.address,
+  );
+  await escrow.deployed();
+
+  // Loop to Approve properties
+  for (let i = 0; i < 3; i++) {
+    let transaction = await realEstate.connect(seller).approve(escrow.address, i + 1);
+    await transaction.wait();
+  }
+
+  // List properties
+  transaction = await escrow.connect(seller).list(1, tokens(20), buyer.address, tokens(10));
+  await transaction.wait();
+
+  transaction = await escrow.connect(seller).list(2, tokens(10), buyer.address, tokens(5));
+  await transaction.wait();
+
+  transaction = await escrow.connect(seller).list(3, tokens(15), buyer.address, tokens(5));
+  await transaction.wait();
+
+  console.log(`Completed deployment of RealEstate and Escrow contracts`);
 
 }
 
