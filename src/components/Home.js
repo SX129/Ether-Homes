0312154a1dd31f450a980ddd1330ca9a8fccd228
Henry Nguyen <nguyenhenry682@gmail.com>
2024-02-28 14:07:10 -0600

    Add home overview section

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,6 +12,23 @@ const Home = ({ home, provider, escrow, togglePop }) => {
                     <img src={home.image} alt='Home' />
                 </div>
 
+                <div className='home__overview'>
+                    <h1>{home.name}</h1>
+                    <p>
+                    <strong>{home.attributes[2].value}</strong> beds |
+                    <strong> {home.attributes[3].value}</strong> baths |
+                    <strong> {home.attributes[4].value}</strong> sqft.
+                    </p>
+                    <p>{home.address}</p>
+                    <h2>{home.attributes[0].value} ETH</h2>
+
+                    <div>
+                        <button className='home__buy'>
+                            Buy
+                        </button>
+                    </div>
+                </div>
+
                 <button onClick={togglePop} className='home__close'>
                     <img src={close} alt="Close" />
                 </button>
