0d109dfe1d07a10f4b60072bb2273ddcb1e7decc
Henry Nguyen <nguyenhenry682@gmail.com>
2024-02-28 00:26:03 -0600

    added account connection functionality to Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,13 @@
 import logo from '../assets/logo.svg';
 
 const Navigation = ({ account, setAccount }) => {
+
+    // Connect to Metamask and obtain current account
+    const connectHandler = async () => {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(accounts[0]);
+    }
+
     return (
         <nav>
             <ul className='nav__links'>
@@ -14,9 +21,12 @@ const Navigation = ({ account, setAccount }) => {
                 <h1>Ether Homes</h1>
             </div>
 
-            <button type='button' className='nav__connect'>
-                0x0...
-            </button>
+            {account ? (
+                <button type='button' className='nav__connect'>{account.slice(0, 6) + '...' + account.slice(38, 42)}</button>
+            ) : (
+                <button type='button' className='nav__connect' onClick={connectHandler}>Connect</button>
+            )}
+
         </nav>
     )
 }
