7774f2fab5b8c2d7b2367fc50b5cb98fc62e520e
Henry Nguyen <nguyenhenry682@gmail.com>
2024-02-28 10:33:47 -0600

    Refactor App component to load blockchain data and display homes

--- src/App.js
+++ src/App.js
@@ -15,11 +15,39 @@ import config from './config.json';
 
 function App() {
 
-  const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState(null)
+  const [escrow, setEscrow] = useState(null)
+
+  const [account, setAccount] = useState(null)
+
+  const [homes, setHomes] = useState([])
+  const [home, setHome] = useState({})
+  const [toggle, setToggle] = useState(false);
 
   // Load blockchain data
-  const loadBlockkchainData = async () => {
+  const loadBlockchainData = async () => {
+    // Connect to Hardhat Network
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const network = await provider.getNetwork();
+
+    // Load Real Estate contract
+    const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider);
+    const totalSupply = await realEstate.totalSupply();
+    const homes = [];
+
+    for (var i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json();
+      homes.push(metadata);
+    }
+    setHomes(homes);
+
+    // Load Escrow contract
+    const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider);
+    setEscrow(escrow);
 
     // Watch for account changes
     window.ethereum.on('accountsChanged', async () => {
@@ -30,7 +58,7 @@ function App() {
   }
 
   useEffect(() => {
-    loadBlockkchainData();
+    loadBlockchainData();
   }, []);
 
   return (
@@ -43,21 +71,24 @@ function App() {
         <hr />
 
         <div className='cards'>
-          <div className='card'>
-            <div className='card__image'>
-              <img src='' alt='Home' />
-            </div>
+          {homes.map((home, index) => (
+            <div className='card' key={index}>
+              <div className='card__image'>
+                <img src='' alt='Home' />
+              </div>
 
-            <div className='card__info'>
-              <h4>1 ETH</h4>
-              <p>
-                <strong>1</strong> beds |
-                <strong> 2</strong> baths |
-                <strong> 3</strong> sqft.
-              </p>
-              <p>1234 Elm St.</p>
+              <div className='card__info'>
+                <h4>1 ETH</h4>
+                  <p>
+                    <strong>1</strong> beds |
+                    <strong> 2</strong> baths |
+                    <strong> 3</strong> sqft.
+                  </p>
+                <p>1234 Elm St.</p>
+              </div>
             </div>
-          </div>
+          ))}
+          
         </div>
 
       </div>
