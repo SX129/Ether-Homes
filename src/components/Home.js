d2ca235bd594240f7403bb696d3120786348b516
Henry Nguyen <nguyenhenry682@gmail.com>
2024-02-28 14:04:35 -0600

    Add home details and close button to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,15 @@ const Home = ({ home, provider, escrow, togglePop }) => {
 
     return (
         <div className="home">
+            <div className='home__details'>
+                <div className='home__image'>
+                    <img src={home.image} alt='Home' />
+                </div>
+
+                <button onClick={togglePop} className='home__close'>
+                    <img src={close} alt="Close" />
+                </button>
+            </div>
         </div>
     );
 }
